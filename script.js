a0612bf487b7bbea28a7ce425b5a565ee9e2fe63
Azat-webdev <62510440+Azat-webdev@users.noreply.github.com>
2021-08-14 18:01:44 +0400

    feat: создали квадрат в DomElement

--- script.js
+++ script.js
@@ -1,30 +1,37 @@
 'use strict';
 
-class DomElement {
-  constructor(selector, height, width, bg, fontSize) {
-    this.selector = selector;
-    this.height = height;
-    this.width = width;
-    this.bg = bg;
-    this.fontSize = fontSize;
-  }
-
-  createElem() {
-    let elem;
-    if (this.selector.substr(0, 1) === '.') {
-      
-      elem = document.createElement('div');
-      elem.classList.add(this.selector.slice(1));
-      
-    } else if (this.selector.substr(0, 1) === '#') {
-      elem = document.createElement('p');
-      elem.setAttribute('id', this.selector.slice(1));
+document.addEventListener("DOMContentLoaded", () => {
+  class DomElement {
+    constructor(selector, height, width, bg, fontSize) {
+      this.selector = selector;
+      this.height = '100px';
+      this.width = '100px';
+      this.bg = 'blue';
+      this.fontSize = '10px';
     }
-    elem.style.cssText =`height:${this.height}px; width: ${this.width}px; background: ${this.bg}; font-size: ${this.fontSize}px`;
-
-    document.body.append(elem);
+  
+    createElem() {
+      let elem;
+      if (this.selector.substr(0, 1) === '.') {
+        
+        elem = document.createElement('div');
+        elem.classList.add(this.selector.slice(1));
+        
+      } else if (this.selector.substr(0, 1) === '#') {
+        elem = document.createElement('p');
+        elem.setAttribute('id', this.selector.slice(1));
+      }
+      elem.style.cssText =`height:${this.height}px; width: ${this.width}px; background: ${this.bg}; font-size: ${this.fontSize}px`;
+  
+      document.body.append(elem);
+    }
+    
   }
-}
+  let square = new DomElement({
+    position: 'absolute'
+  });
+  
+  document.body.appendChild(square.newElem());
 
-let elDiv = new DomElement('.block', 120, 120, 'grey', 20).createElem();
-let elParagraph = new DomElement('#clock', 120, 120, 'blue', 20).createElem();
+});
+
