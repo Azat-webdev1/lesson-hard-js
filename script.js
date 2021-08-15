84c0e6ae071125a793629b9f3ef0a7dc5be6d469
Azat-webdev <62510440+Azat-webdev@users.noreply.github.com>
2021-08-15 14:19:07 +0400

    feat: написал обработчик события keydown

--- script.js
+++ script.js
@@ -1,37 +1,70 @@
 'use strict';
 
-document.addEventListener("DOMContentLoaded", () => {
-  class DomElement {
-    constructor(selector, height, width, bg, fontSize) {
-      this.selector = selector;
-      this.height = '100px';
-      this.width = '100px';
-      this.bg = 'blue';
-      this.fontSize = '10px';
-    }
-  
-    createElem() {
-      let elem;
-      if (this.selector.substr(0, 1) === '.') {
-        
-        elem = document.createElement('div');
-        elem.classList.add(this.selector.slice(1));
-        
-      } else if (this.selector.substr(0, 1) === '#') {
-        elem = document.createElement('p');
-        elem.setAttribute('id', this.selector.slice(1));
-      }
-      elem.style.cssText =`height:${this.height}px; width: ${this.width}px; background: ${this.bg}; font-size: ${this.fontSize}px`;
-  
-      document.body.append(elem);
-    }
-    
+let body = document.querySelector('body');
+let div = document.createElement('div');
+
+class DomElement {
+
+  constructor(selector, height, width, bg, fontSize) {
+    this.selector = selector;
+    this.height = height;
+    this.width = width;
+    this.bg = bg;
+    this.fontSize = fontSize;
   }
-  let square = new DomElement({
-    position: 'absolute'
-  });
+
+  createElem() {
+    
+    let selector = this.selector;
+
+    if (selector[0] === '.') { 
+      div.setAttribute('class', selector.slice(1));
+      body.insertAdjacentElement('afterbegin', div);
+
+      div.style.cssText = `
+        height: ${this.height};
+        width: ${this.width};
+        background: ${this.bg};
+        font-size: ${this.fontSize};
+        position: absolute;
+        text-align: center;
+      `;
+        
+      div.textContent = 'Квадрат';
+    } else if (selector[0] === '#') { 
+      div.setAttribute('id', selector.slice(1));
+      body.insertAdjacentElement('afterbegin', div);
+      div.textContent = 'Квадрат2';
+    }
+  }
+}
+
+const square = new DomElement('.square', '200px', '200px', 'orange', '25px'); 
+
+document.addEventListener('DOMContentLoaded', () => { 
   
-  document.body.appendChild(square.newElem());
+  square.createElem();
+
+  let divLeft = 10;
+  let divTop = 10;
+
+  document.addEventListener('keydown', (event) => {
+    if (event.key === 'ArrowRight') {
+      div.style.marginLeft = divLeft + 'px';
+      divLeft += 10;
+    }
+    if (event.key === 'ArrowLeft') {
+      div.style.marginLeft = divLeft - 10 + 'px';
+      divLeft -= 10;
+    }
+    if (event.key === 'ArrowUp') {
+      div.style.marginTop = divTop - 10 + 'px';
+      divTop -= 10;
+    }
+    if (event.key === 'ArrowDown') {
+      div.style.marginTop = divTop + 'px';
+      divTop += 10;
+    }
+  });
 
 });
-
