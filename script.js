5934d590bbba5798076e7d1885fbba86143ad904
Azat-webdev <62510440+Azat-webdev@users.noreply.github.com>
2021-08-22 14:54:26 +0400

    feat: создал функцию startAnimate, а в ней animate, а также добавил startBtn, stopBtn, resetBtn

--- script.js
+++ script.js
@@ -1,26 +1,56 @@
 'use strict';
 
-const input = document.querySelector('input');
-const p = document.querySelector('.block');
+const startAnimate = () => {
 
+  const startBtn = document.querySelector('#startBtn');
+  const stopBtn = document.querySelector('#stopBtn');
+  const resetBtn = document.querySelector('#resetBtn');
+  const canvas = document.querySelector('#stage');
+  const ctx = canvas.getContext('2d');
 
-const handleInput = (e) => {
-  p.textContent = e.target.value;
+  let requestID;
+
+  ctx.fillStyle = '#212121';
+  
+  stopBtn.style.background = 'red';
+  resetBtn.style.background = '#ffbf00';
+
+  let posX = 0;
+  let boxWidth = 50;
+  let pixelsPerFrame = 5; 
+
+  ctx.fillRect(posX, 0, boxWidth, canvas.height);
+
+  // Animate.
+  const animate = () => {
+    requestID = requestAnimationFrame(animate);
+
+    if (posX <= (canvas.width - boxWidth)) {
+      ctx.clearRect((posX - pixelsPerFrame), 0, boxWidth, canvas.height);
+      ctx.fillRect(posX, 0, boxWidth, canvas.height);
+      posX += pixelsPerFrame;
+    } else {
+      cancelAnimationFrame(requestID);
+    }
+  }
+
+  startBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    requestID = requestAnimationFrame(animate);
+  });
+
+  stopBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    cancelAnimationFrame(requestID);
+  });
+
+  resetBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    posX = 0;
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.fillRect(posX, 0, boxWidth, canvas.height);
+  });
+
 };
 
-const debounce = (fn, ms, ...args) => {
-  let timer;
-  return (value) => {
-    timer = setTimeout(() => {
-      
-      fn.apply(this, [value, ...args]);
-      
-      clearTimeout(timer);
-      
-      timer = null;
-    }, ms);
-  };
-};
-
-input.addEventListener('keyup', debounce(handleInput, 300));
-
+startAnimate();
