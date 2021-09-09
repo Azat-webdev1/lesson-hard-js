b48f7469bf426ef725744e3fbf978f01137b6b8c
Azat-webdev <62510440+Azat-webdev@users.noreply.github.com>
2021-09-09 19:58:37 +0400

    feat: добавил анимацию и метод toDoEdit

---
+++ js/script.js
@@ -0,0 +1,138 @@
+'use strict';
+
+class Todo {
+  constructor(form, input, todoList, todoCompleted, todoContainer) {
+    this.form = document.querySelector(form);
+    this.input = document.querySelector(input);
+    this.todoList = document.querySelector(todoList);
+    this.todoCompleted = document.querySelector(todoCompleted);
+    this.todoContainer = document.querySelector(todoContainer);
+    this.todoData = new Map(JSON.parse(localStorage.getItem('toDoList')));
+
+  }
+
+  addToStorage() {
+    localStorage.setItem('toDoList', JSON.stringify([...this.todoData]));
+  }
+
+  render() {
+    this.todoList.textContent = '';
+    this.todoCompleted.textContent = '';
+    this.todoData.forEach(this.createItem);
+    this.addToStorage();
+  }
+
+  createItem = (item) => {
+    const li = document.createElement('li');
+    li.classList.add('todo-item');
+    li.key = item.key;
+    li.insertAdjacentHTML('beforeend', `
+        <span class="text-todo">${item.value}</span>
+        <div class="todo-buttons">
+          <button class="todo-edit"></button>
+          <button class="todo-remove"></button>
+          <button class="todo-complete"></button>
+        </div>`);
+
+    if (item.completed) {
+      this.todoCompleted.append(li);
+    } else {
+      this.todoList.append(li);
+    }
+    this.input.value = '';
+  }
+
+  addTodo(e) {
+    e.preventDefault();
+    if (this.input.value.trim()) {
+      const newTodo = {
+        value: this.input.value,
+        completed: false,
+        key: this.generateKey(),
+      };
+      this.todoData.set(newTodo.key, newTodo);
+      this.render();
+    } else {
+      alert('Пустое дело добавить нельзя!!');
+    }
+  }
+
+  generateKey() {
+    return Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+  }
+
+  deleteItem(item) {
+    let currentElem = item.closest('.todo-item');
+    this.todoData.forEach((elem, key, map) => {
+      if (key === currentElem.key) {
+        map.delete(key);
+      }
+      this.render();
+    });
+  }
+
+  completedItem(item) {
+    let currentElem = item.closest('.todo-item');
+    this.todoData.forEach((elem, key) => {
+      if (currentElem.key === key) {
+        elem.completed = (!elem.completed) ? true : false;
+      }
+      this.render();
+    });
+  }
+
+  handler() {
+    this.todoContainer.addEventListener('click', (e) => {
+      let target = e.target;
+
+      if (target.matches('.todo-complete')) {
+        this.animate(target.closest('.todo-item'), () => {
+          this.completedItem(target)
+        });
+      } else if (target.matches('.todo-remove')) {
+        this.animate(target.closest('.todo-item'), () => {
+          this.deleteItem(target)
+        });
+      } else if (target.matches('.todo-edit')) {
+        target = target.closest('.todo-item');
+        this.toDoEdit(target);
+      } else {
+        target.blur();
+        return;
+      }
+    });
+  }
+
+  toDoEdit(elem) {
+    const span = elem.querySelector(".text-todo");
+    span.setAttribute("contenteditable", true);
+    span.focus();
+  }
+
+  animate(elem, callback) {
+    let countOpacity = 1;
+    let hideInterval = () => {
+      let animateId = requestAnimationFrame(hideInterval);
+      countOpacity -= 0.1;
+      if (countOpacity > 0) {
+        elem.style.opacity = countOpacity;
+      } else {
+        cancelAnimationFrame(animateId);
+      }
+    }
+    hideInterval();
+    
+    setTimeout(() => {
+      callback();
+    }, 250);
+  }
+  init() {
+    this.form.addEventListener('submit', this.addTodo.bind(this));
+    this.render();
+    this.handler();
+  }
+}
+
+const todo = new Todo('.todo-control', '.header-input', '.todo-list', '.todo-completed', '.todo-container');
+
+todo.init();
